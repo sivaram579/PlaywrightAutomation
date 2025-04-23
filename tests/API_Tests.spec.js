2c61ad109c703e3c55e29e95f05e1689c0bdf173
Sivaram <sivaram579@gmail.com>
2025-04-23 15:06:28 +0530

    API Testing with Playwright

---
+++ tests/API_Tests.spec.js
@@ -0,0 +1,41 @@
+import { test, expect} from '@playwright/test';
+import { request } from 'http';
+
+var userid;
+
+test('Get Users', async ({request}) => {
+    const response = await request.get("https://reqres.in/api/users?page=2");
+    console.log(await response.json());
+    expect(response.status()).toBe(200);
+})
+
+test('Create User', async ({request}) => {
+
+    const response = await request.post("https://reqres.in/api/users",
+                        {
+                            data:{ "name": "Sivaram", "job": "leader" },
+                            headers:{ "Accept":"application/json" }
+                        });
+
+    console.log(await response.json());
+    await expect(response.status()).toBe(201);
+    var res = await response.json();
+    userid = res.id;
+    console.log(userid);
+})
+
+test('Update User', async ({request}) => {
+    const response = await request.put("https://reqres.in/api/users/"+userid,
+        {
+            data:{ "name": "Sivaram", "job": "Engineer" },
+            headers:{ "Accept":"application/json" }
+        });
+
+    console.log(await response.json());
+    await expect(response.status()).toBe(200);    
+})
+
+test('Delete User', async ({request}) => {
+    const response = await request.delete("https://reqres.in/api/users/"+userid)    
+    await expect(response.status()).toBe(204)
+})
