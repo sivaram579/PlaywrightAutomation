611a91bbc90f040ba19f48b0220fc12ee006ad27
Sivaram <sivaram579@gmail.com>
2025-04-23 10:53:59 +0530

    Hard and Soft Assertions in Playwright

---
+++ tests/Assertions.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Hard and soft assertions', async ({page}) => {
+
+    await page.goto("https://demoblaze.com/")
+
+    //Hard Assertions - Execution stops at the failed assertion
+   /* await expect(page).toHaveTitle("STORE123");
+    await expect(page).toHaveURL("https://demoblaze.com/");
+    await expect(page.locator('#nava')).toBeVisible();
+    */
+    //Soft Assertions - Execution will continue even after failed assertion
+    await expect.soft(page).toHaveTitle("STORE123");
+    await expect.soft(page).toHaveURL("https://demoblaze.com/");
+    await expect.soft(page.locator('#nava')).toBeVisible();
+
+})
